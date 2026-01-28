43465e112969903ea2f834df7940bb5cd66bc958
leoo-c1 <chinchillaleo01@gmail.com>
2026-01-28 20:01:52 +1100

    Used numpy arrays to store prices, tracked runtime

--- software/price_parser.py
+++ software/price_parser.py
@@ -1,11 +1,32 @@
-filename = "data/WDC_tickbidask.csv"
+'''
+This is the python code for parsing our market data.
 
-print("\nString manipulation begin:")
-print(f"Opening {filename}...")
+It first reads from the .csv containing market data, and extracts prices from each line.
+We multiply by 100 to turn our dollars and cents combo into a single integer.
+Prices may include more than 2 digits after the decimal point, so we force the result of 100*price to be an integer.
+
+'''
+import numpy as np
+import time
+
+start_time = time.perf_counter()
+
+filename = "data/WDC_sample.csv"        # Market data file
+number_of_ticks = 49999                    # Number of data points for price that we want to store
+prices = np.zeros(number_of_ticks)      # Create empty np array with enough size for our price data
+
 try:
-    with open(filename, 'r') as f:
+    with open(filename, 'r') as f:              # Open our market data
         # Find price for first 20 lines
-        for i in range (21):
-            print(f"Price {i}: ${f.readline().strip().split(",",3)[2]}")
+        for i in range (number_of_ticks):
+            prices[i] = f.readline().strip().split(',',3)[2]              # Add each line's price to our prices array
+            # print(f"Price {i}: ${f.readline().strip().split(',',3)[2]}")    # On each line, extract the price
 except FileNotFoundError:
     print("Error: File not found.")
+
+end_time = time.perf_counter()
+
+# Calculate the elapsed time
+elapsed_time = end_time - start_time
+
+print(elapsed_time)
