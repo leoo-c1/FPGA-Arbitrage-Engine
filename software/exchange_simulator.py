5586517fc90c1ccc606c019c53e5a8fce3f4411e
leoo-c1 <chinchillaleo01@gmail.com>
2026-01-31 11:21:50 +1100

    Removed note

--- software/exchange_simulator.py
+++ software/exchange_simulator.py
@@ -1,20 +1,3 @@
-'''
-This is the python code for parsing our market data and simulating arbitrage opportunities by
-creating lag between two Exchanges (A and B) during volatile price changes in Exchange A.
-
-It first reads from the .csv containing market data, and extracts prices from each line.
-We multiply by 100 to turn our dollars and cents combo into a single integer.
-Prices may include more than 2 digits after the decimal point, so we force the result of 100*price to be an integer.
-
-UART packet structure:
-Byte 0: Header, indicates beginning of price transmission (0xAA)
-Byte 1: Upper 8 bits of Exchange A's price
-Byte 2: Lower 8 bits of Exchange A's price
-Byte 3: Upper 8 bits of Exchange B's price
-Byte 4: Lower 8 bits of Exchange B's price
-Byte 5: Footer, indicates end of price transmission (0x55)
-'''
-
 import time
 from collections import deque
 import random
